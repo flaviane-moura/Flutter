e0948a6e142f9ace4c3dd02ce0f1e29e81546aaf
flaviane-moura <flavianecax@gmail.com>
2023-03-05 19:24:58 -0300

    Consumo Médio (ObservableList) com MobX

---
+++ consumo_medio/lib/pages/home.dart
@@ -0,0 +1,94 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, library_private_types_in_public_api, unnecessary_string_interpolations
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import '../stores/consumo_list.dart';
+final consumos = ConsumoList();
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+
+  double consumo = 0;
+
+  void _handleConsumoField(String text){
+    setState(() {
+      if(text != ''){
+        consumo = double.parse(text);
+      } else {
+        consumo = 0;
+      }
+    });
+
+  }
+
+  void _handleRegistrarButton (){
+    if(consumo > 0){
+      consumos.addConsumo(consumo);
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World')
+      ),
+      body: Observer(
+        builder: (_) => Column(
+          children: [
+            TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: 'Qual foi o consumo da viagem?'
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: _handleConsumoField
+            ), 
+            ElevatedButton(
+              onPressed: _handleRegistrarButton, 
+              child: Text('Registrar $consumo')
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text('Consumos Registrados'),
+                      Text('${consumos.list.length}')
+                    ],
+                  )
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text('Consumos Médio Geral'),
+                      Text('${consumos.mediaGeral}')
+                    ],
+                  )
+                )
+              ],
+            ),
+            Text('Registros:'),
+            Container(
+              height: 200,
+              child: ListView.builder(
+                itemCount: consumos.list.length,
+                itemBuilder: (BuildContext ctx, int index){
+                  return Text('- ${consumos.list[index]}');
+                }
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+}
---
+++ consumo_medio/lib/stores/consumo_list.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_is_empty, avoid_function_literals_in_foreach_calls
+
+import 'package:mobx/mobx.dart';
+
+part 'consumo_list.g.dart';
+
+class ConsumoList = _ConsumoList with _$ConsumoList;
+
+abstract class _ConsumoList with Store {
+  @observable
+  ObservableList<double> list = ObservableList<double>();
+
+  @computed
+  String get mediaGeral {
+    if(list.length > 0) {
+      double total = 0;
+      list.forEach((element) {
+        total = total + element;
+      });
+
+      return (total / list.length).toStringAsFixed(2);
+    } else {
+      return '0.00';
+    }
+  }
+
+  @action
+  void addConsumo(double data) {
+    list.add(data);
+  }
+
+  @action
+  void limpar() => list = ObservableList<double>();
+
+}
